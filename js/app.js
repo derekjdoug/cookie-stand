68f6d71ed1ec68b60a5f64afbe0c146509438d8a
Derek Douglas <derek.j.douglas13@gmail.com>
2022-03-02 23:00:05 -0600

    finished seattleStore object JavaScript and fixed html and css url paths

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const salmonSales = document.getElementById('salmonSales');
+console.log(salmonSales);
+
 let hour = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm','2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 let seattleStore = {
@@ -30,10 +33,31 @@ let seattleStore = {
     let dailyTotal = Math.round(this.cookiesSoldPerHour.reduce((a, b) => a + b));
     this.dailySale = dailyTotal;
     return this.dailySale;
+  },
+  render: function () {
+    this.cookiesSoldPerCustomerCalc();
+    this.cookieSaleAlgorithm();
+    this.totalCookies();
+    let j = 0;
+    for (j = 0; j < hour.length; j++) {
+      let clock = document.createElement('li');
+      clock.textContent = `${hour[j]}: ${this.cookiesSoldPerHour[j]}`;
+      salmonSales.appendChild(clock);
+    }
+    let tot = document.createElement('li');
+    tot.textContent = `Total: ${this.dailySale}`;
+    salmonSales.appendChild(tot);
   }
 };
 
-console.log(seattleStore.customerCount());
-console.log(seattleStore.cookiesSoldPerCustomerCalc());
-console.log(seattleStore.cookieSaleAlgorithm());
-console.log(seattleStore.totalCookies());
+// console.log(seattleStore.customerCount());
+// console.log(seattleStore.cookiesSoldPerCustomerCalc());
+// console.log(seattleStore.cookieSaleAlgorithm());
+// console.log(seattleStore.totalCookies());
+seattleStore.render();
+
+// const salmonSales = document.getElementById('salmonSales');
+
+// let hourByHourSales = document.createElement('article');
+
+// salmonSales.appendChild(hourByHourSales);
