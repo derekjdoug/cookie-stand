044a2c6f10c54d68df2d0099562b2a9222172ed9
Derek Douglas <derek.j.douglas13@gmail.com>
2022-03-02 21:56:10 -0600

    updated JavaScript object functionality and html pages

--- js/app.js
+++ js/app.js
@@ -13,12 +13,27 @@ let seattleStore = {
     return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
   },
   cookiesSoldPerCustomerCalc: function () {
-    return this.customerCount() * this.avgCookieSale;
+    return Math.round(this.customerCount() * this.avgCookieSale);
   },
   cookieSaleAlgorithm: function () {
-
+    //calc cookies for all 14 hours
+    let i = 0;
+    for (i = 0; i < hour.length; i++) {
+      let customerThisHour = this.cookiesSoldPerCustomerCalc();
+      this.cookiesSoldPerHour.push(customerThisHour);
+      // console.log(customerThisHour);
+      // console.log(this.cookiesSoldPerHour);
+    }
+    return this.cookiesSoldPerHour;
+  },
+  totalCookies: function () {
+    let dailyTotal = Math.round(this.cookiesSoldPerHour.reduce((a, b) => a + b));
+    this.dailySale = dailyTotal;
+    return this.dailySale;
   }
 };
 
 console.log(seattleStore.customerCount());
 console.log(seattleStore.cookiesSoldPerCustomerCalc());
+console.log(seattleStore.cookieSaleAlgorithm());
+console.log(seattleStore.totalCookies());
